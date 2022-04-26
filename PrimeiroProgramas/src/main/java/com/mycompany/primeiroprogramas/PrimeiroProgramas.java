5d563e6476b34883feb809bdccd110b46a1ccc8c
Manuel Duarte <manuelgarcia.duarte1@gmail.com>
2022-04-26 19:16:32 -0300

    Atualização Java

--- PrimeiroProgramas/src/main/java/com/mycompany/primeiroprogramas/PrimeiroProgramas.java
+++ PrimeiroProgramas/src/main/java/com/mycompany/primeiroprogramas/PrimeiroProgramas.java
@@ -3,13 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package org.yourcompany.yourproject;
+package com.mycompany.primeiroprogramas;
 
 /**
  *
  * @author manue
  */
-public class Testenovo {
+public class PrimeiroProgramas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
