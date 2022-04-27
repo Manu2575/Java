39b384588e86a9f6312260260c5e7f2625eb7d2b
Manuel Duarte <manuelgarcia.duarte1@gmail.com>
2022-04-27 12:52:51 -0300

    Estudando Java

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposprimitivos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author manue
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o nome do aluno: ");
+        String nome = teclado.nextLine();
+        System.out.print("Digite a nota do aluno: ");
+        float nota = teclado.nextFloat();
+        //System.out.println("A nota é" + nota);
+        //System.out.printf("A nota de %s e %.2f \n", nome, nota);
+        System.out.format("A nota de %s e %.2f \n", nome, nota);
+    }
+    
+}
