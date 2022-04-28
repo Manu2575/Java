3b200b865098c6cf05c824fe30476378db5148c1
Manuel Duarte <manuelgarcia.duarte1@gmail.com>
2022-04-28 17:23:46 -0300

    Estudando Java

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author manue
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A media e igual a " + m);*/
+        
+        /*int numero = 5
+        numero ++;
+        System.out.println(numero);*/
+        
+        /*int numero = 10
+        int valor = 4 + numero ++;
+        System.out.ptintln(valor);
+        System.out.ptintln(numero);*/
+        
+        /*int x = 4;
+        x *= 2;
+        System.out.println(x);*/
+        
+        /*float v = 8.3f;
+        int ar = (int) Math.ceil(v);
+        System.out.println(ar);*/
+        
+        /*double ale = Math.random();
+        System.out.println(ale);*/
+        
+        double ale = Math.random();
+        int n = (int)( 5 + ale *(10-5));
+        System.out.println(n);
+    }
+    
+}
