4bdc6cfe4ec93ae629d81e25ba95a3f672f8dd04
Manuel Duarte <manuelgarcia.duarte1@gmail.com>
2022-05-05 19:06:07 -0300

    Estudando Java

---
+++ Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor01;
+
+/**
+ *
+ * @author manue
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[]= {3,2,8,7,5,4};
+        for(int c=0; c<=5; c++){
+            System.out.println("Na posicao " + c + " temos o valor " + n[c]);
+        }
+    }
+    
+}
---
+++ Vetor03/src/vetor03/Vetor03.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetor03;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author manue
+ */
+public class Vetor03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double v[] = {3.5, 2.75, 9, -4.5};
+        Arrays.sort(v);
+        for (double valor: v){
+            System.out.print(valor + " ");
+        }
+    }
+    
+}
